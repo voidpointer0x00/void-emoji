2718a1bc18488fc280099da3308781596b0b563c
Vasiliy <void.pointer@ya.ru>
2023-09-04 13:10:11 +0500

    fix: add no-args constructor

--- src/main/java/ru/voidpointer/paper/voidemoji/config/Emoji.java
+++ src/main/java/ru/voidpointer/paper/voidemoji/config/Emoji.java
@@ -1,13 +1,15 @@
 package ru.voidpointer.paper.voidemoji.config;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.configurate.objectmapping.ConfigSerializable;
 
 @Data
-@ConfigSerializable
+@NoArgsConstructor
 @RequiredArgsConstructor
+@ConfigSerializable
 public class Emoji {
     @NotNull private String glyph;
 }
